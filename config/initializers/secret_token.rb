b5aaa33e42764fec1ea2f9705071fb9b39c77170
kitaindia <morisot64@gmail.com>
2012-08-26 12:13:19 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoasterNotifier::Application.config.secret_token = 'acacbc1a522eddb98a118b7ccfc4a1806d007d9a8a3f91cfbcec6bb687f04f0c096da007a7b78cdcaaddf1795544af04a04b2361886955faa8b5a7f821f68cc2'
