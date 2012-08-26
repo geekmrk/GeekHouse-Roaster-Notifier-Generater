fd3ab396a54e249d1447409027f3675dff712a45
kitaindia <morisot64@gmail.com>
2012-08-26 16:41:47 +0900

    index消した

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'Pa9TtDB2XpU3HoAVrjlQ', 'KZYXJOIThQTUH160GdfLgzW3eM8hjbl6AeR2haTTOw'
+  provider :twitter, '', ''
 end
